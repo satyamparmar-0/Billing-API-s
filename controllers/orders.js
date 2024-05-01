ba0fe83ff74702d9d8bed2107defbb5d59ca5c13
nidhika ahirwa <nidhikaahirwar@gmail.com>
2024-05-01 17:38:07 +0530

    order updates

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,7 +1,8 @@
 const Order = require('../models/orders');
 const Product = require('../models/product');
+const User = require('../models/user');
 
-exports.createOrder = async (req, res) => {
+const createOrder = async (req, res) => {
     try {
         const { user_id, items, status } = req.body;
         let orderItems = [];
@@ -37,3 +38,72 @@ exports.createOrder = async (req, res) => {
         
     }
 };
+
+const updateOrder = async (req, res) => {
+    try {
+        const { order_id, user_id, items, status } = req.body;
+
+    
+        const existingOrder = await Order.findById(order_id);
+        if (!existingOrder) {
+            return res.status(404).json({ error: "Order not found" });
+        }
+        const user = await User.findById(user_id);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        let totalAmount = 0;
+        const orderItems = [];
+
+        for (const item of items) {
+            const { product_id, quantity } = item;
+            const product = await Product.findById(product_id);
+            if (!product) {
+                return res.status(404).json({ error: "Product not found" });
+            }
+
+            const price = product.price * quantity;
+            totalAmount += price;
+
+            orderItems.push({
+                product_id,
+                product_name: product.name,
+                quantity,
+                price
+            });
+        }
+        existingOrder.user_id = user_id;
+        existingOrder.items = orderItems;
+        existingOrder.total_amount = totalAmount;
+        existingOrder.status = status;
+
+        // Save the updated order
+        await existingOrder.save();
+        res.status(200).json({ message: 'Order updated successfully', order: existingOrder });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+const getAllOrdersByUserId = async (req, res) => {
+    const { user_id } = req.body;
+    try {
+        const orders = await Order.find({ user_id });
+        if (!orders) {
+            return res.status(404).json("No Orders Found for this User");
+        }
+        res.json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+module.exports = {
+    updateOrder,
+    createOrder,
+    getAllOrdersByUserId,
+}
--- routes/orders.js
+++ routes/orders.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const orderController = require('../controllers/orders');
 
 // Routes for product CRUD operations
-router.post('/createOrder', orderController.createOrder);
-
+// router.post('/createOrder',orderController.createOrder);
+router.put('/updateOrder',orderController.updateOrder);
+router.post('/createOrder',orderController.createOrder);
+router.get('/getByUserId',orderController.getAllOrdersByUserId);
 module.exports = router;
